bcfd75d19d9090936b8a9001d593e41022c8d83d
JithanRoy <jonyyy81@gmail.com>
2022-03-11 22:51:39 +0600

    first commit

---
+++ BulkyBook/Models/Catagory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Models
+{
+    public class Catagory : Controller
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
